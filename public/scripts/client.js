a99c5fc003ee525cb616951df84d311590b76220
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-09-08 14:14:05 -0400

    Create function to render tweets one after the other

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,17 +6,38 @@
 
 $(document).ready(function() {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  //takes in an array of tweet objects and append each one to #tweets-container
+  const renderTweeets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
   
   //returns tweet <article> element containing the entire HTML structure of the tweet
   const createTweetElement = function(tweetData) {
@@ -41,8 +62,6 @@ $(document).ready(function() {
 
     return $tweet;
   };
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  $('#tweets-container').append($tweet);
+
+  renderTweeets(data);
 });
